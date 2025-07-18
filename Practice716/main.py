952c5cf55e7605ef04072a19cb3db264dde92172
Alex <xrlogic1@gmail.com>
2025-07-18 12:50:50 +0400

    Practice

---
+++ Practice716/main.py
@@ -0,0 +1,21 @@
+# # x = "Hello world"
+# #
+# # newList = x[1:5]
+# #
+# # print(x)
+# # print(newList)
+#
+# # list comprehension
+# l = [i * 2 for i in range(5) if i != 2]
+#
+# # l = []
+# #
+# # for i in range(5):
+# #     if i != 2:
+# #         l.append(i * 2)
+#
+# print(l)
+
+
+
+
---
+++ Practice718/main.py
@@ -0,0 +1,42 @@
+# i = 0
+#
+# while True:
+#     if i == 10:
+#         break
+#
+#     print(i)
+#
+#     i += 1
+##########
+# a = 42
+#
+# b = str(a)
+#
+# print(b * 2)
+##########
+#
+# string = "Hello World"
+#
+# strLen = len(string)
+#
+#
+# for i in range(strLen):
+#     print(string[i])
+#
+# name = input()
+#
+# print(f"Hello, my name is {name}")
+
+
+size = 5
+i = 0
+while i < size:
+    j = 0
+
+    while j < size:
+        print("* ", end='')
+
+        j += 1
+    print()
+
+    i += 1
